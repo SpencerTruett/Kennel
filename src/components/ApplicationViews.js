53b4080823f686b8167871882f59bae2dcf9871c
Spencer Truett <srtruett1993@gmail.com>
2020-01-27 13:26:55 -0600

    added mock authentication

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -8,36 +8,52 @@ import { CustomerProvider } from "./customer/CustomerProvider"
 import CustomerList from "./customer/CustomerList"
 import { EmployeeProvider } from "./employee/EmployeeProvider"
 import EmployeeList from "./employee/EmployeeList"
+import EmployeeForm from "./employee/EmployeeForm"
+// import AnimalDetails from "./animal/AnimalDetails"
 
 export default (props) => {
     return (
         <>
             <LocationProvider>
                 {/* Render the location list when http://localhost:3000/ */}
-                <Route exact path="/">
-                    <LocationList />
-                </Route>
+                <EmployeeProvider>
+                  <Route exact path="/">
+                      <LocationList />
+                  </Route>
+                </EmployeeProvider>
             </LocationProvider>
 
             <AnimalProvider>
                 {/* Render the animal list when http://localhost:3000/animals */}
-                <Route path="/animals">
-                    <AnimalList />
-                </Route>
+                <LocationProvider>
+                    <CustomerProvider>
+                        <Route exact path="/animals">
+                            <AnimalList />
+                        </Route>
+                        {/* <Route path="/animals/:animalId(\d+)" render={
+                            props => <AnimalDetails {...props} />
+                        } /> */}
+                    </CustomerProvider>
+                </LocationProvider>
             </AnimalProvider>
 
             <CustomerProvider>
-                {/* Render the animal list when http://localhost:3000/customers */}
+                {/* Render the customer list when http://localhost:3000/customers */}
                 <Route path="/customers">
                     <CustomerList />
                 </Route>
             </CustomerProvider>
 
             <EmployeeProvider>
-                {/* Render the animal list when http://localhost:3000/employees */}
-                <Route path="/employees">
-                    <EmployeeList />
-                </Route>
+                {/* Render the employee list when http://localhost:3000/employees */}
+                <LocationProvider>
+                    <Route exact path="/employees" render={
+                        props => <EmployeeList {...props} />
+                    } />
+                    <Route path="/employees/create" render={
+                        props => <EmployeeForm {...props} />
+                    } />
+                </LocationProvider>
             </EmployeeProvider>
         </>
     )
--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,16 +1,36 @@
 import React, { useContext } from "react"
 import { EmployeeContext } from "./EmployeeProvider"
+import { LocationContext } from "../location/LocationProvider"
 import Employee from "./Employee"
 import "./Employee.css"
 
-export default () => {
+export default props => {
     const { employees } = useContext(EmployeeContext)
+    const { locations } = useContext(LocationContext)
 
     return (
         <div className="employees">
-        {
-            employees.map(emp => <Employee key={emp.id} employee={emp} />)
-        }
+            <h1>Employees</h1>
+            <button onClick={() => props.history.push("/employees/create")}>
+                Add Employee
+            </button>
+            <article className="employeeList">
+                {
+                    employees.map(employee => {
+                        // Find this employee's matching location object
+                        const foundedLocation = locations.find(
+                            (location) => {
+                                return location.id === employee.locationId
+                            }
+                        )
+
+                        // Pass the matching location to Employee component
+                        return <Employee key={employee.id}
+                                         location={foundedLocation}
+                                         employee={employee} />
+                    })
+                }
+            </article>
         </div>
     )
 }
